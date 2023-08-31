9b3dfa8a810c695ed0d940f16ae44d3d473a40d3
Dirga Prakesha <dirga.prakesha@mekari.com>
2023-08-31 16:33:24 +0700

    feat(template): add mekari partners templates

---
+++ templates/mekari-partners/mekari-partners-dashboard/src/views/Offers.vue
@@ -0,0 +1,286 @@
+<template>
+  <mp-box>
+    <Header />
+    <mp-flex as="main" max-height="calc(100vh - 60px)">
+      <Sidebar is-alternate is-custom />
+      <mp-box
+        as="section"
+        data-id="content"
+        width="100%"
+        height="calc(100vh - 60px)"
+        overflow-y="auto"
+        background-color="background"
+      >
+        <mp-flex
+          justify="space-between"
+          align-items="center"
+          padding-x="6"
+          padding-y="1.125rem"
+        >
+          <mp-box>
+            <mp-heading as="h1" font-size="2xl" font-weight="bold" color="dark">
+              Programs
+            </mp-heading>
+          </mp-box>
+        </mp-flex>
+        <mp-box
+          min-height="calc(100vh - 132px)"
+          border-top-width="1px"
+          border-left-width="1px"
+          border-top-left-radius="md"
+          border-color="gray.100"
+          background-color="white"
+          padding="6"
+        >
+          <mp-flex justify-content="space-between" margin-bottom="6">
+            <mp-flex gap="4">
+              <mp-box>
+                <mp-select value="all" width="56">
+                  <option value="all">All program</option>
+                </mp-select>
+              </mp-box>
+              <mp-box>
+                <mp-select value="all" width="56">
+                  <option value="all">All status</option>
+                </mp-select>
+              </mp-box>
+            </mp-flex>
+
+            <mp-box>
+              <mp-input-group>
+                <mp-input-left-addon :with-background="false">
+                  <mp-icon name="search" size="sm" />
+                </mp-input-left-addon>
+                <mp-input placeholder="Search..." width="56" />
+              </mp-input-group>
+            </mp-box>
+          </mp-flex>
+
+          <mp-table-container width="full" max-width="full" overflow="scroll">
+            <mp-table>
+              <mp-table-head>
+                <mp-table-row cursor="pointer">
+                  <mp-table-cell as="th">
+                    Program
+                    <mp-icon name="sort-default" />
+                  </mp-table-cell>
+                  <mp-table-cell as="th">
+                    Product
+                    <mp-icon name="sort-default" />
+                  </mp-table-cell>
+                  <mp-table-cell as="th">
+                    Total clicks
+                    <mp-icon name="sort-default" />
+                  </mp-table-cell>
+                  <mp-table-cell as="th">
+                    Total lead
+                    <mp-icon name="sort-default" />
+                  </mp-table-cell>
+                  <mp-table-cell as="th">
+                    Total won
+                    <mp-icon name="sort-default" />
+                  </mp-table-cell>
+                  <mp-table-cell as="th">
+                    Conversion rate
+                    <mp-icon name="sort-default" />
+                  </mp-table-cell>
+                  <mp-table-cell as="th" colspan="2">
+                    Status
+                    <mp-icon name="sort-default" />
+                  </mp-table-cell>
+                </mp-table-row>
+              </mp-table-head>
+              <mp-table-body>
+                <mp-table-row>
+                  <mp-table-cell as="td">Jurnal Reseller Program</mp-table-cell>
+                  <mp-table-cell as="td">Jurnal</mp-table-cell>
+                  <mp-table-cell as="td">320</mp-table-cell>
+                  <mp-table-cell as="td">10</mp-table-cell>
+                  <mp-table-cell as="td">5</mp-table-cell>
+                  <mp-table-cell as="td">50%</mp-table-cell>
+                  <mp-table-cell as="td">
+                    <mp-badge variant="subtle" variant-color="green">
+                      Active
+                    </mp-badge>
+                  </mp-table-cell>
+                  <mp-table-cell as="td" text-align="right">
+                    <mp-button
+                      as="router-link"
+                      to="/offers-detail"
+                      variant="outline"
+                      >See detail</mp-button
+                    >
+                  </mp-table-cell>
+                </mp-table-row>
+                <mp-table-row>
+                  <mp-table-cell as="td">Qontak Reseller Program</mp-table-cell>
+                  <mp-table-cell as="td">Qontak</mp-table-cell>
+                  <mp-table-cell as="td">320</mp-table-cell>
+                  <mp-table-cell as="td">10</mp-table-cell>
+                  <mp-table-cell as="td">5</mp-table-cell>
+                  <mp-table-cell as="td">50%</mp-table-cell>
+                  <mp-table-cell as="td">
+                    <mp-badge variant="subtle" variant-color="green">
+                      Active
+                    </mp-badge>
+                  </mp-table-cell>
+                  <mp-table-cell as="td" text-align="right">
+                    <mp-button variant="outline">See detail</mp-button>
+                  </mp-table-cell>
+                </mp-table-row>
+              </mp-table-body>
+            </mp-table>
+          </mp-table-container>
+
+          <Pagination />
+
+          <mp-divider orientation="horizontal" margin-y="6" />
+
+          <mp-box>
+            <mp-heading as="h2" font-size="xl">
+              Add more product offer
+            </mp-heading>
+            <mp-text color="gray.600">
+              Quick register to offer more product for our Mekari products
+            </mp-text>
+          </mp-box>
+
+          <mp-flex gap="6" margin-top="5" margin-bottom="6">
+            <mp-flex
+              flex="1 0 0 "
+              background="white"
+              padding-x="4"
+              padding-y="3"
+              border-radius="md"
+              border-width="1px"
+              flex-direction="column"
+            >
+              <mp-icon name="talenta-brand" margin-bottom="3" />
+              <mp-text font-weight="semibold" margin-bottom="2">
+                Mekari Talenta
+              </mp-text>
+              <mp-text color="gray.600" font-size="sm" margin-bottom="6">
+                HRIS & Payroll automation Software
+              </mp-text>
+              <mp-text is-link>Join partners</mp-text>
+            </mp-flex>
+            <mp-flex
+              flex="1 0 0 "
+              background="white"
+              padding-x="4"
+              padding-y="3"
+              border-radius="md"
+              border-width="1px"
+              flex-direction="column"
+            >
+              <mp-icon name="klikpajak-brand" margin-bottom="3" />
+              <mp-text font-weight="semibold" margin-bottom="2">
+                Mekari Klikpajak
+              </mp-text>
+              <mp-text color="gray.600" font-size="sm" margin-bottom="6">
+                Online tax management Software
+              </mp-text>
+              <mp-text is-link>Join partners</mp-text>
+            </mp-flex>
+            <mp-flex
+              flex="1 0 0 "
+              background="white"
+              padding-x="4"
+              padding-y="3"
+              border-radius="md"
+              border-width="1px"
+              flex-direction="column"
+            >
+              <mp-icon name="sign-brand" margin-bottom="3" />
+              <mp-text font-weight="semibold" margin-bottom="2">
+                Mekari e-Sign
+              </mp-text>
+              <mp-text color="gray.600" font-size="sm" margin-bottom="6">
+                Document authorization software
+              </mp-text>
+              <mp-text is-link>Join partners</mp-text>
+            </mp-flex>
+            <mp-flex
+              flex="1 0 0 "
+              background="white"
+              padding-x="4"
+              padding-y="3"
+              border-radius="md"
+              border-width="1px"
+              flex-direction="column"
+            >
+              <mp-icon name="flex-brand" margin-bottom="3" />
+              <mp-text font-weight="semibold" margin-bottom="2">
+                Mekari Flex
+              </mp-text>
+              <mp-text color="gray.600" font-size="sm" margin-bottom="6">
+                Employee Benefit & Welfare Software
+              </mp-text>
+              <mp-text is-link>Join partners</mp-text>
+            </mp-flex>
+          </mp-flex>
+
+          <mp-flex>
+            <mp-text color="gray.600" margin-right="2"
+              >Looking for other products?
+            </mp-text>
+            <mp-text is-link>See more</mp-text>
+          </mp-flex>
+        </mp-box>
+      </mp-box>
+    </mp-flex>
+  </mp-box>
+</template>
+
+<script>
+import {
+  MpBox,
+  MpFlex,
+  MpIcon,
+  MpHeading,
+  MpText,
+  MpButton,
+  MpBadge,
+  MpSelect,
+  MpInput,
+  MpInputGroup,
+  MpInputLeftAddon,
+  MpTableContainer,
+  MpTable,
+  MpTableHead,
+  MpTableBody,
+  MpTableRow,
+  MpTableCell,
+  MpDivider,
+} from "@mekari/pixel";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Pagination from "../components/Pagination";
+
+export default {
+  name: "Offers",
+  components: {
+    MpBox,
+    MpFlex,
+    MpIcon,
+    MpHeading,
+    MpText,
+    MpButton,
+    MpBadge,
+    MpSelect,
+    MpInput,
+    MpInputGroup,
+    MpInputLeftAddon,
+    MpTableContainer,
+    MpTable,
+    MpTableHead,
+    MpTableBody,
+    MpTableRow,
+    MpTableCell,
+    MpDivider,
+    Pagination,
+    Header,
+    Sidebar,
+  },
+};
+</script>
---
+++ templates/mekari-partners/mekari-partners-dashboard/src/views/PersonalInfo.vue
@@ -0,0 +1,213 @@
+<template>
+  <mp-box>
+    <Header />
+    <mp-flex as="main" max-height="calc(100vh - 60px)">
+      <Sidebar is-alternate is-custom />
+      <mp-box
+        as="section"
+        data-id="content"
+        width="100%"
+        height="calc(100vh - 60px)"
+        overflow-y="auto"
+        background-color="background"
+      >
+        <mp-flex
+          justify="space-between"
+          align-items="center"
+          padding-x="6"
+          padding-y="1.125rem"
+        >
+          <mp-box>
+            <mp-heading as="h1" font-size="2xl" font-weight="bold" color="dark">
+              Personal info
+            </mp-heading>
+          </mp-box>
+        </mp-flex>
+        <mp-box
+          min-height="calc(100vh - 132px)"
+          border-top-width="1px"
+          border-left-width="1px"
+          border-top-left-radius="md"
+          border-color="gray.100"
+          background-color="white"
+          padding="6"
+        >
+          <mp-flex gap="6">
+            <mp-box width="50%">
+              <mp-flex justify-content="space-between" align-items="baseline">
+                <mp-heading as="h2" font-size="xl" margin-bottom="5">
+                  Account details
+                </mp-heading>
+                <mp-flex gap="2" align-items="center">
+                  <mp-text is-link>Request change</mp-text>
+                  <mp-icon name="newtab" size="sm" variant="duotone" />
+                </mp-flex>
+              </mp-flex>
+
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">ID</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>27804529</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">Company name</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>PT Central Perk Indonesia</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">Partner name</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>Rizal Chandra</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">Email</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>rizalchandra@cpi.com</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">Phone number</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>0812290922</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">Join date</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>12 Jan 2022</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">ID Type</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>KTP, Personal NPWP</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">ID Number</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>3200291000251</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">ID Tax number</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>93.972.215.4-542.000</mp-text>
+                </mp-flex>
+              </mp-flex>
+              <mp-flex>
+                <mp-flex flex="3 0 0%" padding-y="2">
+                  <mp-text color="gray.600">Address</mp-text>
+                </mp-flex>
+                <mp-flex flex="4 0 0%" padding-y="2">
+                  <mp-text>
+                    Jl Kebon Bibit No.75/42 Bandung Wetan, Tamansari. Kota
+                    Bandung
+                  </mp-text>
+                </mp-flex>
+              </mp-flex>
+            </mp-box>
+            <mp-box width="50%">
+              <mp-heading as="h2" font-size="xl" margin-bottom="5">
+                Show getting started
+              </mp-heading>
+
+              <mp-toggle is-checked>
+                Quick start guide on how to start partner with us
+              </mp-toggle>
+            </mp-box>
+          </mp-flex>
+
+          <mp-divider orientation="horizontal" margin-y="6" />
+
+          <mp-box width="50%">
+            <mp-flex justify-content="space-between" align-items="baseline">
+              <mp-heading as="h2" font-size="xl" margin-bottom="5">
+                Payment info
+              </mp-heading>
+              <mp-flex gap="2" align-items="center">
+                <mp-text is-link>Request change</mp-text>
+                <mp-icon name="newtab" size="sm" variant="duotone" />
+              </mp-flex>
+            </mp-flex>
+
+            <mp-flex>
+              <mp-flex flex="3 0 0%" padding-y="2">
+                <mp-text color="gray.600">Bank account number</mp-text>
+              </mp-flex>
+              <mp-flex flex="4 0 0%" padding-y="2">
+                <mp-text>BCA</mp-text>
+              </mp-flex>
+            </mp-flex>
+            <mp-flex>
+              <mp-flex flex="3 0 0%" padding-y="2">
+                <mp-text color="gray.600">Bank account holder</mp-text>
+              </mp-flex>
+              <mp-flex flex="4 0 0%" padding-y="2">
+                <mp-text>Rizal Chandra</mp-text>
+              </mp-flex>
+            </mp-flex>
+            <mp-flex>
+              <mp-flex flex="3 0 0%" padding-y="2">
+                <mp-text color="gray.600">Bank account number</mp-text>
+              </mp-flex>
+              <mp-flex flex="4 0 0%" padding-y="2">
+                <mp-text>730940022</mp-text>
+              </mp-flex>
+            </mp-flex>
+          </mp-box>
+        </mp-box>
+      </mp-box>
+    </mp-flex>
+  </mp-box>
+</template>
+
+<script>
+import {
+  MpBox,
+  MpFlex,
+  MpHeading,
+  MpText,
+  MpIcon,
+  MpToggle,
+  MpDivider,
+} from "@mekari/pixel";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+
+export default {
+  name: "PersonalInfo",
+  components: {
+    MpBox,
+    MpFlex,
+    MpHeading,
+    MpText,
+    MpIcon,
+    MpToggle,
+    MpDivider,
+    Header,
+    Sidebar,
+  },
+};
+</script>
