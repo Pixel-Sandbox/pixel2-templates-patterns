860df0b40715ed081fb4ad364c2cbbc8b5343c2e
Dimas Raka Septiawan <dimasrakas@gmail.com>
2025-01-13 08:16:06 +0700

    feat: add table sticky and fullscreen pattern

---
+++ patterns/table-sticky-and-fullscreen/src/components/parts/TableStickyContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <mp-box position="absolute" :visibility="isVisible ? 'visible' : 'hidden'">
+    <mp-box
+      position="fixed"
+      :top="placement === 'top' ? topOffset : 'auto'"
+      :bottom="placement === 'bottom' ? topOffset : 'auto'"
+      z-index="13"
+      bg="gray.50"
+      box-shadow="rgb(208, 214, 221) 0px 1px"
+    >
+      <mp-box
+        position="relative"
+        left="0px"
+        width="full"
+        display="flex"
+        flex-direction="column"
+        transition="transform 0.3s ease"
+      >
+        <mp-box overflow="hidden">
+          <slot />
+        </mp-box>
+      </mp-box>
+    </mp-box>
+  </mp-box>
+</template>
+
+<script>
+import { MpBox } from "@mekari/pixel";
+export default {
+  components: {
+    MpBox,
+  },
+  props: {
+    isVisible: {
+      type: Boolean,
+      default: false,
+    },
+    placement: {
+      type: String,
+      default: "top",
+    },
+    topOffset: {
+      type: String,
+      default: "0px",
+    },
+  },
+};
+</script>
